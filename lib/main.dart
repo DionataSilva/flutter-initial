f2a7b38287d497390690220d0ba7e38267041e0e
Dionata Santos da Silva <dionatasantos.silva@gmail.com>
2020-03-05 01:03:34 -0300

    Step 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
 
     return MaterialApp(
         title: 'Flutter Demo',
@@ -16,8 +16,21 @@ class MyApp extends StatelessWidget {
             title: Text('Welcome to Flutter'),
           ),
           body: Center(
-            child: Text(wordPair.asPascalCase),
+            child: RandomWords(),
           ),
         ));
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
